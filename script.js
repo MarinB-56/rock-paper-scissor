408ffc5d815e4a5bea6b57d08a44d800c40d234b
150065021+MarinB-56@users.noreply.github.com <Marinus@MacBook-Pro-de-Marin.local>
2024-01-07 13:18:07 +0100

    Adding DOM modifications and UI

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 let scorePlayer = 0, scoreComputer = 0;
 
+//Managing buttons
+const btn__rock = document.querySelector('#rock');
+const btn__paper = document.querySelector('#paper');
+const btn__scissor = document.querySelector('#scissor');
+
+//Managing div for result
+const div__result = document.querySelector('#result');
+const div__score = document.querySelector('#score');
+
+btn__rock.addEventListener('click', () => {
+    //Comparing player and computer choice
+    div__result.textContent = playRound( "rock" , getComputerChoice() );
+    div__score.textContent = `Player :  ${scorePlayer} - ${scoreComputer} : Computer`;
+});
+btn__paper.addEventListener('click', () => {
+    //Comparing player and computer choice
+    div__result.textContent = playRound( "paper" , getComputerChoice() );
+    div__score.textContent = `Player :  ${scorePlayer} - ${scoreComputer} : Computer`;
+});
+btn__scissor.addEventListener('click', () => {
+    //Comparing player and computer choice
+    div__result.textContent = playRound( "scissor" , getComputerChoice() );
+    div__score.textContent = `Player :  ${scorePlayer} - ${scoreComputer} : Computer`;
+});
+
+
 function getComputerChoice(){
     let choice = Math.floor( Math.random()*3 )+1;
     switch(choice){
@@ -17,8 +43,6 @@ function playRound(playerSelection , computerSelection){
     //Case insensitive
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-
-    //console.log(playerSelection + " " + computerSelection);
 
     //Even result 
     if(computerSelection == playerSelection){
@@ -66,9 +90,8 @@ function game(){
     while(true){
         //Launch the game
         do{
-            
-            alert(result = playRound( prompt("Jouez : Rock, Paper or Scissor") , getComputerChoice()) + `\nVotre score : ${scorePlayer} / Ordinateur : ${scoreComputer}`); 
-        
+            alert(result = playRound( prompt("Jouez : Rock, Paper or Scissor") 
+            , getComputerChoice()) + `\nVotre score : ${scorePlayer} / Ordinateur : ${scoreComputer}`); 
         }while(result == "Egalité"); //While even score
 
         //Vérifier si l'un des joueurs a gagné
@@ -81,4 +104,4 @@ function game(){
     }
 }
 
-alert(game());
+//alert(game());
